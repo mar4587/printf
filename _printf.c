516f03176b91dccf9a5f9081f16c7c23285f9786
mar4587 <marshetnigatu@yahoo.com>
2023-09-19 06:50:51 -0700

    Task mo

--- _printf.c
+++ _printf.c
@@ -3,9 +3,10 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - This is a printf function
+ * @format: the format
+ * Return: the printed chars
+ *
  */
 int _printf(const char *format, ...)
 {
@@ -53,10 +54,12 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - This prints the contents of the buffer if it exist
  * @buffer: Array of chars
+ *
  * @buff_ind: Index at which to add next char, represents the length.
  */
+
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
